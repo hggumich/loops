f9034cfc82d328dbbb7b9ad6bf6cb4c450cbb161
Hector Garza <hggumich@gmail.com>
2022-09-25 11:15:47 -0400

    finish loops

--- src/main.rs
+++ src/main.rs
@@ -30,4 +30,30 @@ fn main() {
         count_two += 1;
     }
     println!("End count_two = {count_two}");
+
+    let mut number_three = 3;
+
+    while number_three != 0 {
+        println!("{number_three}");
+        number_three -= 1;
+    }
+
+    println!("LIFTOFF!!!!");
+
+    let a = [10, 20, 30, 40, 50];
+    let mut index_four = 0;
+
+    while index_four < 5 {
+        println!("the value is: {}", a[index_four]);
+        index_four += 1;
+    }
+
+    for element_four in a {
+        println!("the value is: {element_four}");
+    }
+
+    for number_five in (1..4).rev() {
+        println!("{number_five}!");
+    }
+    println!("LIFT OFF!!!!!");
 }
