152deac4f7bc24f40d2be468148d975d2b2d0fa2
Hector Garza <hggumich@gmail.com>
2022-09-25 11:03:48 -0400

    start with while loops

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
 fn main() {
-    println!("Hello, world!");
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+    println!("The result is {result}");
+
+    // second loop example
+
+    let mut count_two = 0;
+    'counting_up: loop {
+        println!("count_two = {count_two}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count_two == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+        count_two += 1;
+    }
+    println!("End count_two = {count_two}");
 }
